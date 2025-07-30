7d2fbccdf87d0946a6407f34f431cc654a3777aa
comerun <571038609@qq.com>
2025-07-30 17:48:37 +0800

    Front-end page creation

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,4 @@
 // 创建问卷按钮点击事件
-document.addEventListener('DOMContentLoaded', function() {
-  const createBtn = document.getElementById('createBtn');
-  if (createBtn) {
-    createBtn.addEventListener('click', function() {
-      alert('即将跳转到问卷创建页面');
-      // window.location.href = '/create-questionnaire.html';
-    });
-  }
-});
+ document.querySelector('.btn-create').addEventListener('click', function() {
+     window.location.href = 'manual-create-questionnaire.html';
+ });
