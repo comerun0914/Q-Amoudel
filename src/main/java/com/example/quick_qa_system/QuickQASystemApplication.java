0e7cbf20fb2610695ce08cfb81c090348ae11599
comerun0914 <571038609@qq.com>
2025-07-30 12:02:29 +0800

    back create

---
+++ src/main/java/com/example/quick_qa_system/QuickQASystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.quick_qa_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickQASystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickQASystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quick_qa_system/QuickQASystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick_qa_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickQASystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
